6f448fd6a6f51149c5f59957b2e1bba0ddc09b79
josh182014 <josh182014@gmail.com>
2019-04-08 14:44:40 -0600

    updated js file to match spec

--- js/index.js
+++ js/index.js
@@ -40,3 +40,52 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let services = document.querySelectorAll('a');
+services[0].textContent = siteContent["nav"]["nav-item-1"];
+services[1].textContent = siteContent["nav"]["nav-item-2"];
+services[2].textContent = siteContent["nav"]["nav-item-3"];
+services[3].textContent = siteContent["nav"]["nav-item-4"];
+services[4].textContent = siteContent["nav"]["nav-item-5"];
+services[5].textContent = siteContent["nav"]["nav-item-6"];
+
+let ctaH1 = document.querySelector('h1');
+ctaH1.textContent = siteContent.cta.h1;
+
+let button1 = document.querySelector('button');
+button1.textContent = siteContent.cta.button;
+
+let ctaImg = document.getElementById("cta-img");
+ctaImg.setAttribute('src', siteContent["cta"]["img-src"])
+
+let toph4 = document.querySelectorAll('.top-content .text-content h4')
+toph4[0].textContent = siteContent["main-content"]["features-h4"];
+toph4[1].textContent = siteContent["main-content"]["about-h4"];
+
+let toContent = document.querySelectorAll('.top-content .text-content p')
+toContent[0].textContent = siteContent["main-content"]["features-content"];
+toContent[1].textContent = siteContent["main-content"]["about-content"];
+
+let middleImg = document.getElementById("middle-img");
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+let bottomh4 = document.querySelectorAll('.bottom-content .text-content h4');
+bottomh4[0].textContent = siteContent["main-content"]["services-h4"];
+bottomh4[1].textContent = siteContent["main-content"]["product-h4"];
+bottomh4[2].textContent = siteContent["main-content"]["vision-h4"];
+
+let bottomContent = document.querySelectorAll('.bottom-content .text-content p');
+bottomContent[0].textContent = siteContent["main-content"]["services-content"];
+bottomContent[1].textContent = siteContent["main-content"]["product-content"];
+bottomContent[2].textContent = siteContent["main-content"]["vision-content"];
+
+let contacth4 = document.querySelector('.contact h4');
+contacth4.textContent = siteContent['contact']['contact-h4'];
+
+let contact = document.querySelectorAll('.contact p');
+contact[0].textContent = siteContent['contact']['address'];
+contact[1].textContent = siteContent['contact']['phone'];
+contact[2].textContent = siteContent['contact']['email'];
+
+let footer = document.querySelector('footer');
+footer.textContent = "Copyright Great Idea! 2018"
